e2c53359b87fb94edf5e7f6ca4a25e8c2fb5df8f
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-03-24 00:41:42 +0900

    first commit
    from /usr/local/etc/xdg/awesome/rc.lua and https://github.com/nedludd/awesome

---
+++ rc.managed.lua
@@ -0,0 +1,26 @@
+-- the path to your file
+local rc, err = loadfile(os.getenv("HOME").."/.config/awesome/awesome.lua")
+if rc then
+	rc, err = pcall(rc);
+	if rc then
+		-- All ok, return.
+		return;
+	end
+end
+
+-- Problems, load fallback rc.lua
+
+-- Default config
+dofile("/usr/local/etc/xdg/awesome/rc.lua")
+
+-- Display the error in the promptbox on all screens
+local awful = require("awful")
+for s = 1,screen.count() do
+	mypromptbox[s].text = awful.util.escape(err:match("[^\n]*"));
+end
+
+-- Write full error (plus traceback) to a file
+local f = io.open(os.getenv("HOME").."/.config/awesome/log/startup.err", "w+")
+f:write("Awesome error during startup on ", os.date("%c:\n\n"))
+f:write(err, "\n");
+f:close();
